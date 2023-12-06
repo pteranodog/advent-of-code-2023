f94daa87cdc081b31d6be6b9c1a2ffea09a20c40
Drew Early <61104507+pteranodog@users.noreply.github.com>
2023-12-06 01:06:53 -0600

    Test inputs and some tests

--- src/main.rs
+++ src/main.rs
@@ -35,3 +35,78 @@ fn main() {
     };
     println!("{}", answer);
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn day01p1() {
+        let input = std::fs::read_to_string("input-test/day01p1.txt").unwrap();
+        assert_eq!(super::day01p1::puzzle_1(input), 142);
+    }
+
+    #[test]
+    fn day01p2() {
+        let input = std::fs::read_to_string("input-test/day01p2.txt").unwrap();
+        assert_eq!(super::day01p2::puzzle_2(input), 281);
+    }
+
+    #[test]
+    fn day02p1() {
+        let input = std::fs::read_to_string("input-test/day02.txt").unwrap();
+        assert_eq!(super::day02p1::puzzle_1(input), 8);
+    }
+
+    #[test]
+    fn day02p2() {
+        let input = std::fs::read_to_string("input-test/day02.txt").unwrap();
+        assert_eq!(super::day02p2::puzzle_2(input), 2286);
+    }
+
+    #[test]
+    fn day03p1() {
+        let input = std::fs::read_to_string("input-test/day03.txt").unwrap();
+        assert_eq!(super::day03p1::puzzle_1(input), 4361);
+    }
+
+    #[test]
+    fn day03p2() {
+        let input = std::fs::read_to_string("input-test/day03.txt").unwrap();
+        assert_eq!(super::day03p2::puzzle_2(input), 467835);
+    }
+
+    #[test]
+    fn day04p1() {
+        let input = std::fs::read_to_string("input-test/day04.txt").unwrap();
+        assert_eq!(super::day04p1::puzzle_1(input), 13);
+    }
+
+    #[test]
+    fn day04p2() {
+        let input = std::fs::read_to_string("input-test/day04.txt").unwrap();
+        assert_eq!(super::day04p2::puzzle_2(input), 30);
+    }
+
+    #[test]
+    fn day05p1() {
+        let input = std::fs::read_to_string("input-test/day05.txt").unwrap();
+        assert_eq!(super::day05::puzzle_1(input), 35);
+    }
+
+    #[test]
+    fn day05p2() {
+        let input = std::fs::read_to_string("input-test/day05.txt").unwrap();
+        assert_eq!(super::day05::puzzle_2(input), 46);
+    }
+
+    #[test]
+    fn day06p1() {
+        let input = std::fs::read_to_string("input-test/day06.txt").unwrap();
+        assert_eq!(super::day06::puzzle_1(input), 288);
+    }
+
+    #[test]
+    fn day06p2() {
+        let input = std::fs::read_to_string("input-test/day06.txt").unwrap();
+        assert_eq!(super::day06::puzzle_2(input), 71503);
+    }
+}
